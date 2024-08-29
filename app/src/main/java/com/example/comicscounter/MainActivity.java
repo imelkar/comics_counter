c3edc22d21d4a5b80542b21ea2870df45d4ea9cb
imelkar <dianazubareva@gmail.com>
2024-08-29 15:11:14 +0100

    Moving all java to 1 file

--- app/src/main/java/com/example/comicscounter/MainActivity.java
+++ app/src/main/java/com/example/comicscounter/MainActivity.java
@@ -1,25 +1,27 @@
 package com.example.comicscounter;
 
-import static java.time.MonthDay.now;
-
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import androidx.preference.PreferenceManager;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Collections;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
+import java.time.LocalDate;
+
 public class MainActivity extends AppCompatActivity {
 
-//    SharedPreferences sharedPref = getSharedPreferences("mypref",0);
+    Context cc = this;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -27,11 +29,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putInt("date", (now().getDayOfMonth()));
-        editor.putInt("month", (now().getMonthValue()));
+        editor.putInt("date", Integer.parseInt((LocalDate.now()).toString().substring(8)));
+        editor.putInt("month", Integer.parseInt((LocalDate.now()).toString().substring(5, 7)));
         editor.putInt("today", 0);
         editor.putInt("total", 0);
         editor.putInt("min", 0);
@@ -41,6 +42,23 @@ public class MainActivity extends AppCompatActivity {
         editor.apply();
 
         printEverything();
+
+        Button plus_button = new Button(this);
+
+        plus_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences1 = PreferenceManager.getDefaultSharedPreferences(cc);
+                int today = sharedPreferences1.getInt("today", 0);
+
+                SharedPreferences.Editor editor1 = sharedPreferences1.edit();
+
+                editor1.putInt("today", today + 1);
+                editor1.apply();
+                System.out.println("Button clicked");
+
+                printToday();
+            }
+        });
     }
 
     @Override
@@ -48,6 +66,8 @@ public class MainActivity extends AppCompatActivity {
         super.onRestart();
         printEverything();
     }
+
+
 
     protected void printEverything() {
         printDate();
@@ -59,35 +79,40 @@ public class MainActivity extends AppCompatActivity {
         printButton();
     }
 
-    protected void printText(TextView tv, int corx, int cory) {
-//        tv.setX(corx);
-//        tv.setY(cory);
+    protected void printText(TextView tv) {
         tv.setVisibility(View.VISIBLE);
     }
 
+    @SuppressLint("SetTextI18n")
     protected void printDate() {
         TextView date_text = this.findViewById(R.id.date);
-        printText(date_text, 20, 20);
+        printText(date_text);
 
-        Date date = new Date();
-        int number = date.getDate();
-        int month = date.getMonth() + 1;
-        date_text.setText("Дата: " + number + "/" + month);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            date_text.setText((LocalDate.now()).toString());
+        }
+        else {
+            date_text.setText("SDK error getting date");
+        }
     }
 
+    @SuppressLint("SetTextI18n")
     protected void printToday() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         int today = sharedPreferences.getInt("today", 0);
+        System.out.println(today);
         TextView today_text = this.findViewById(R.id.today);
-        printText(today_text, 20, 80);
+        printText(today_text);
         today_text.setText("Сегодня: " + today);
     }
 
+
+    @SuppressLint("SetTextI18n")
     protected void printTotal() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         TextView total_text = this.findViewById(R.id.total);
 
-        printText(total_text, 20, 140);
+        printText(total_text);
         int total = sharedPreferences.getInt("total", 0);
 //        SharedPreferences.Editor editor= sharedPreferences.edit();
 //        editor.putInt("total", total+1);
@@ -95,18 +120,20 @@ public class MainActivity extends AppCompatActivity {
         total_text.setText("Всего: " + total);
     }
 
+    @SuppressLint("SetTextI18n")
     protected void printMin() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         TextView min_text = this.findViewById(R.id.min);
-        printText(min_text, 20, 200);
+        printText(min_text);
         int min = sharedPreferences.getInt("min", 0);
         min_text.setText("Минимум: " + min);
     }
 
+    @SuppressLint("SetTextI18n")
     protected void printMax() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         TextView max_text = this.findViewById(R.id.max);
-        printText(max_text, 20, 260);
+        printText(max_text);
         int max = sharedPreferences.getInt("max", 0);
         max_text.setText("Максимум: " + max);
     }
@@ -114,10 +141,10 @@ public class MainActivity extends AppCompatActivity {
     protected void printArray() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         TextView array_text = this.findViewById(R.id.array);
-        Set<String> set = sharedPreferences.getStringSet("array", new HashSet<String>());
+        Set<String> set = sharedPreferences.getStringSet("array", new HashSet<>());
 //        ArrayList<Integer> array_nums = new ArrayList<>();
         Object[] array = set.toArray();
-        printText(array_text, 20, 380 + set.size() * 60);
+        printText(array_text);
         array_text.setText("Список:\n");
         try {
             array_text.append(String.valueOf(array[0]));
@@ -126,9 +153,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void printButton(){
-        Plus_Button plus_button = new Plus_Button();
-
-//        plus_button.setY(400);
+        //
     }
 
     protected void add1ToToday(){
